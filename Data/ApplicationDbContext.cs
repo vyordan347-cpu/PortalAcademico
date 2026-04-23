9b5a389a0557784bad182bf12e433960419a5b69
vyordan347 <vyordan347@gmail.com>
2026-04-22 22:11:18 -0500

    feat: bootstrap del dominio con cursos, matriculas y seed inicial

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,70 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PortalAcademico.Models;
 
-namespace PortalAcademico.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+namespace PortalAcademico.Data
 {
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Curso>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+
+                entity.Property(c => c.Codigo)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.HasIndex(c => c.Codigo)
+                    .IsUnique();
+
+                entity.Property(c => c.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(c => c.Activo)
+                    .HasDefaultValue(true);
+
+                entity.ToTable(t =>
+                {
+                    t.HasCheckConstraint("CK_Curso_Creditos_MayorQueCero", "\"Creditos\" > 0");
+                    t.HasCheckConstraint("CK_Curso_CupoMaximo_MayorQueCero", "\"CupoMaximo\" > 0");
+                    t.HasCheckConstraint("CK_Curso_HorarioInicio_MenorHorarioFin", "\"HorarioInicio\" < \"HorarioFin\"");
+                });
+            });
+
+            builder.Entity<Matricula>(entity =>
+            {
+                entity.HasKey(m => m.Id);
+
+                entity.Property(m => m.UsuarioId)
+                    .IsRequired();
+
+                entity.Property(m => m.FechaRegistro)
+                    .IsRequired();
+
+                entity.Property(m => m.Estado)
+                    .IsRequired();
+
+                entity.HasOne(m => m.Curso)
+                    .WithMany(c => c.Matriculas)
+                    .HasForeignKey(m => m.CursoId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasIndex(m => new { m.CursoId, m.UsuarioId })
+                    .IsUnique();
+            });
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,19 +4,31 @@ using PortalAcademico.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                      ?? "Data Source=app.db";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await SeedData.InicializarAsync(services);
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,23 +36,21 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
